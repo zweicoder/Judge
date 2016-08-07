53045d52f0e7e23423a1596240e50deb32248fd9
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2016-08-07 15:41:52 +0800

    Fix confusion between bytes and bytes32. Rename Data to Array for more specific and clear naming

--- contracts/CheapArray.sol
+++ contracts/CheapArray.sol
@@ -1,17 +1,17 @@
 // http://ethereum.stackexchange.com/questions/3373/how-to-clear-large-arrays-without-blowing-the-gas-limit
 library CheapArray {
-    struct Data {
-        bytes[] elems;
+    struct Array {
+        bytes32[] elems;
         uint n;
     }
 
-    function insertAll(Data storage self, bytes[] values) internal {
+    function insertAll(Array storage self, bytes32[] values) internal {
         for (uint i=0; i < values.length; i++) {
             insert(self, values[i]);
         }
     }
 
-    function insert(Data storage self, bytes value) internal {
+    function insert(Array storage self, bytes32 value) internal {
         if(self.n == self.elems.length) {
             self.n = self.elems.push(value);
             return;
@@ -19,11 +19,11 @@ library CheapArray {
         self.elems[self.n] = value;
     }
 
-    function clear(Data storage self) internal {
+    function clear(Array storage self) internal {
         self.n = 0;
     }
 
-    function isEmpty(Data storage self) constant returns(bool) {
+    function isEmpty(Array storage self) constant returns(bool) {
         return self.n ==0;
     }
 }
