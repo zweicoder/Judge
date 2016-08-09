afdffc9252be40d719cd8a6ef9ffa9ba05295891
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2016-08-10 00:32:45 +0800

    Deploy JudgeSubImpl for testing purposes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 module.exports = function(deployer) {
 
     deployer.deploy(CheapArrayLib);
+    // deployer.deploy(JudgeSubscriber);
     deployer.autolink();
     if (process.env.NODE_ENV == 'test'){
         deployer.deploy(CheapArrayLibTest)
+        deployer.deploy(JudgeSubImpl)
     }
     deployer.deploy(Judge);
 };
