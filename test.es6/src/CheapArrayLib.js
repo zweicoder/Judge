e4b01cc438be660857df046bb70e61c8194168c1
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2016-08-11 18:04:59 +0800

    Refactoring

--- test.es6/src/CheapArrayLib.js
+++ test.es6/src/CheapArrayLib.js
@@ -1,111 +1,109 @@
 contract('CheapArrayLib', (accounts) => {
-    const elems = ['1', '2', '3'].map((e) => web3.sha3(e))
+  const elems = ['1', '2', '3'].map((e) => web3.sha3(e))
 
-    it('should start with an empty array', () => {
-        const cheapArray = CheapArrayLibTest.deployed()
-        return checkSizeEquals(cheapArray, 0)()
-            .then(checkIsEmpty(cheapArray, true))
-            .then(()=>{
-                return cheapArray.getAll.call()
-            })
-            .then((res)=>{
-                assert.deepEqual(res, [])
-            })
-    })
+  it('should start with an empty array', () => {
+    const cheapArray = CheapArrayLibTest.deployed()
+    return cheapArray.getAll.call()
+      .then((res) => {
+        assert.deepEqual(res, [])
+      })
+      .then(checkSizeEquals(cheapArray, 0))
+      .then(checkIsEmpty(cheapArray, true))
+  })
 
-    it('should insertAll elements', () => {
-        const cheapArray = CheapArrayLibTest.deployed()
-        return cheapArray.insertAll(elems)
-            .then(() => {
-                return Promise.all(
-                    elems.map((e, idx) => {
-                        return cheapArray.get.call(idx)
-                            .then((res) => {
-                                assert.equal(e, res)
-                            })
-                    })
-                )
-            })
-    })
+  it('should insertAll elements', () => {
+    const cheapArray = CheapArrayLibTest.deployed()
+    return cheapArray.insertAll(elems)
+      .then(() => {
+        const promises = elems.map((e, idx) => {
+          return cheapArray.get.call(idx)
+        })
 
-    it('should return the correct size', () => {
-        const cheapArray = CheapArrayLibTest.deployed()
-        return checkSizeEquals(cheapArray, elems.length, 'Size should update after inserting')()
-            .then(checkIsEmpty(cheapArray, false, 'isEmpty should return false'))
-    })
+        return Promise.all(promises)
+      })
+      .then((actual) => assert.deepEqual(actual, elems))
+  })
 
-    it('should clear elements', () => {
-        const cheapArray = CheapArrayLibTest.deployed()
-        return cheapArray.clear()
-            .then(checkSizeEquals(cheapArray, 0, 'Size should be 0 after clearing'))
-            .then(checkIsEmpty(cheapArray, true, 'isEmpty should return true'))
-    })
+  it('should return the correct size', () => {
+    const cheapArray = CheapArrayLibTest.deployed()
+    return checkSizeEquals(cheapArray, elems.length, 'Size should update after inserting')()
+      .then(checkIsEmpty(cheapArray, false, 'isEmpty should return false'))
+  })
 
-    it('should throw if idx for get(idx) is out of bounds', () => {
-        const cheapArray = CheapArrayLibTest.deployed()
-        return cheapArray.get(0)
-            .then((r) => {
-                assert.fail(0, 1)
-            })
-            .catch((e) => {
-                assert.isOk(e)
-            })
-    })
+  it('should clear elements', () => {
+    const cheapArray = CheapArrayLibTest.deployed()
+    return cheapArray.clear()
+      .then(checkSizeEquals(cheapArray, 0, 'Size should be 0 after clearing'))
+      .then(checkIsEmpty(cheapArray, true, 'isEmpty should return true'))
+  })
 
-    it('should still insert properly after clearing', () => {
-        const cheapArray = CheapArrayLibTest.deployed()
-        const anotherItem = web3.sha3('4')
-        const allItems = elems.concat(anotherItem)
-        return cheapArray.insertAll(elems)
-            .then(() => {
-                return cheapArray.insert(anotherItem)
-            })
-            .then(checkSizeEquals(cheapArray, elems.length + 1))
-            .then(checkIsEmpty(cheapArray, false))
-            .then(() => {
-                return Promise.all(
-                    allItems.map((e, idx) => {
-                        return cheapArray.get.call(idx)
-                            .then((res) => {
-                                assert.equal(e, res)
-                            })
-                    })
-                )
-            })
-    })
+  it('should throw if idx for get(idx) is out of bounds', () => {
+    const cheapArray = CheapArrayLibTest.deployed()
+    return cheapArray.get(0)
+      .then((r) => {
+        assert.fail(0, 1)
+      })
+      .catch((e) => {
+        assert.isOk(e)
+      })
+  })
 
-    it('should return all elements with getAll()', () => {
-        const cheapArray = CheapArrayLibTest.deployed()
-        const anotherItem = web3.sha3('4')
-        const allItems = elems.concat(anotherItem)
-        return cheapArray.getAll.call()
-            .then((res)=>{
-                assert.deepEqual(res, allItems)
-            })
-    })
+  it('should still insert properly after clearing', () => {
+    const cheapArray = CheapArrayLibTest.deployed()
+    const anotherItem = web3.sha3('4')
+    const allItems = elems.concat(anotherItem)
+    return cheapArray.insertAll(elems)
+      .then(() => {
+        return cheapArray.insert(anotherItem)
+      })
+      .then(checkSizeEquals(cheapArray, elems.length + 1))
+      .then(checkIsEmpty(cheapArray, false))
+      .then(() => {
+        return Promise.all(
+          allItems.map((e, idx) => {
+            return cheapArray.get.call(idx)
+              .then((res) => {
+                assert.equal(e, res)
+              })
+          })
+        )
+      })
+  })
+
+  it('should return all elements with getAll()', () => {
+    const cheapArray = CheapArrayLibTest.deployed()
+    const anotherItem = web3.sha3('4')
+    const allItems = elems.concat(anotherItem)
+    return cheapArray.getAll.call()
+      .then((res) => {
+        assert.deepEqual(res, allItems)
+      })
+  })
 })
 
 function checkSizeEquals(arr, n, msg) {
-    if (!msg) msg = ''
-    return () => {
-        return arr.getSize.call()
-            .then((res) => {
-                assert.equal(res.toNumber(), n, msg)
-            })
-    }
+  if (!msg)
+    msg = ''
+  return () => {
+    return arr.getSize.call()
+      .then((res) => {
+        assert.equal(res.toNumber(), n, msg)
+      })
+  }
 }
 
 function checkIsEmpty(arr, bool, msg) {
-    if (!msg) msg = ''
-    return () => {
-        return arr.isEmpty.call()
-            .then((res) => {
-                if (bool) {
-                    assert.isOk(res, msg)
-                    return
-                }
+  if (!msg)
+    msg = ''
+  return () => {
+    return arr.isEmpty.call()
+      .then((res) => {
+        if (bool) {
+          assert.isOk(res, msg)
+          return
+        }
 
-                assert.isNotOk(res, msg)
-            })
-    }
+        assert.isNotOk(res, msg)
+      })
+  }
 }
